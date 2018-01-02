94163dacab4b433384de01cb0f7b767f89f65aa0
Divyanshu Singh <dsinecos@gmail.com>
2018-01-02 16:22:21 +0530

    Create constructor functions for Linked List and Node

---
+++ linkedList.js
@@ -0,0 +1,13 @@
+// Create constructor functions for Linked List and Node
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+
+}
