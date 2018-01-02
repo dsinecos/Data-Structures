29df67fad9008296f43ea723e6189b64f176403e
Divyanshu Singh <dsinecos@gmail.com>
2018-01-02 16:40:58 +0530

    Implement addToHead method for a LinkedList

--- linkedList.js
+++ linkedList.js
@@ -11,3 +11,22 @@ function LinkedList() {
     this.tail = null;
 
 }
+
+// addToHead 
+// Attaching the head and tail when using addToHead method
+// If LL is empty both will point at the recently added node
+// If LL is not empty, head will point at the recently added node whereas tail will remain unchanged
+// The prev pointer of the node that was previously pointed to by this.head should point to the recently added node
+
+LinkedList.prototype.addToHead = function (value) {
+    var newNode = new Node(value, this.head, null);
+
+    if (this.head) {
+        this.head.prev = newNode;
+    } else {
+        this.tail = newNode;
+    }
+
+    this.head = newNode;
+}
+
